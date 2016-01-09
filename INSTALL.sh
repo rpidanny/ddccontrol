dc76a9c76a4ab2dd0eb11b69d0b79d6c88d299e2
rpidanny <abhishekmaharjan1993@gmail.com>
2016-01-09 20:35:02 +0545

    ddccontrol-db

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,1 +1,7 @@
 #!/bin/bash
+
+sudo apt-get install -y i2c-dev
+sudo apt-get install -y xbacklight
+
+wget http://ftp.cn.debian.org/debian/pool/main/d/ddccontrol-db/ddccontrol-db_20061014-4_all.deb
+sudo dpkg -i ddccontrol-db_20061014-4_all.deb
